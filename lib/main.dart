dd9f37813ef5d5c2aa78cd0ed080086e260aeabf
NguyenTheNhan <nhanntse140758@fpt.edu.vn>
2021-06-17 20:59:08 +0700

    Add login, profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:bike_for_rent/pages/login_screen.dart';
+import 'package:bike_for_rent/pages/profile/profile_screen.dart';
 import 'package:bike_for_rent/pages/return_bike_map.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ReturnBikeMap());
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfileScreen(),
+    );
+  }
 }
