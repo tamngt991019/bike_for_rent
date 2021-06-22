66a2593f3859b8665dbdf7191ae78d0fc38db8f3
TamNT <tamngt991019@gmail.com>
2021-06-22 11:01:12 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,4 @@
-import 'package:bike_for_rent/pages/bike_get_map.dart';
-import 'package:bike_for_rent/pages/history.dart';
-import 'package:bike_for_rent/pages/history_detail.dart';
 import 'package:bike_for_rent/pages/home.dart';
-import 'package:bike_for_rent/pages/login.dart';
-import 'package:bike_for_rent/pages/personal.dart';
-import 'package:bike_for_rent/pages/rating.dart';
-import 'package:bike_for_rent/pages/rent_bike_detail.dart';
-import 'package:bike_for_rent/pages/rent_bike_filter.dart';
-import 'package:bike_for_rent/pages/rent_bike_list.dart';
-import 'package:bike_for_rent/pages/bike_return_map.dart';
-import 'package:bike_for_rent/test_api/test.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/rent_bike_manager.dart';
-
-void main() => runApp(MaterialApp(home: Test()));
+void main() => runApp(MaterialApp(home: Home()));
