d019f0b2e1cf6099e4b3adfb7350b0e1ae9ba8fd
NguyenTheNhan <nhanntse140758@fpt.edu.vn>
2021-07-15 19:57:41 +0700

    update code from master

---
+++ lib/widgets/test_booking_card.dart
@@ -0,0 +1,102 @@
+import 'package:bike_for_rent/models/bike_image_model.dart';
+import 'package:bike_for_rent/models/bike_model.dart';
+import 'package:bike_for_rent/models/booking_model.dart';
+import 'package:bike_for_rent/services/bike_image_service.dart';
+import 'package:bike_for_rent/widgets/booking_detail.dart';
+import 'package:bike_for_rent/widgets/test_booking_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:bike_for_rent/constants/my_colors.dart' as my_colors;
+
+class BookingCardTest extends StatefulWidget {
+  final BikeModel bikeModel;
+  final BookingModel bookingModel;
+  final List<String> listImage;
+
+  BookingCardTest({
+    Key key,
+    this.bikeModel,
+    this.bookingModel,
+    this.listImage,
+  }) : super(key: key);
+
+  @override
+  _BookingCardTestState createState() => _BookingCardTestState();
+}
+
+class _BookingCardTestState extends State<BookingCardTest> {
+  List<String> imageUrls() {
+    List<String> imageUrls = [
+      "https://media.publit.io/file/BikeForRent/banner/banner1.jpg",
+      "https://media.publit.io/file/BikeForRent/banner/banner2.jpg",
+      "https://media.publit.io/file/BikeForRent/banner/banner3.jpg",
+      "https://media.publit.io/file/BikeForRent/banner/banner4.jpg",
+      "https://media.publit.io/file/BikeForRent/banner/banner5.jpg",
+    ];
+    return imageUrls;
+  }
+
+  // BikeImageService bikeImageService = new BikeImageService();
+
+  // List<BikeImageModel> bikeImageList;
+
+  // Future loadListBikeImagesByBikeType(String bileId) {
+  //   if (bikeImageList == null) {
+  //     bikeImageList = [];
+  //   }
+  //   Future<List<BikeImageModel>> futureCases =
+  //       bikeImageService.getBikeImageModels();
+  //   futureCases.then((list) {
+  //     this.bikeImageList = list;
+  //   });
+  //   return futureCases;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.bikeModel == null) {
+      return Center(
+        child: Text("Không có xe nào phù hợp"),
+      );
+    } else {
+      return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.only(top: 5, bottom: 10),
+        child: Column(
+          children: [
+            // Hình ảnh xe
+            ImageSlideshow(
+              width: double.infinity,
+              height: 200,
+              initialPage: 0,
+              indicatorColor: my_colors.primary,
+              indicatorBackgroundColor: Colors.white,
+              children: imageUrls()
+                  .map((img) => ClipRRect(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                      child: Image.network(img, fit: BoxFit.cover)))
+                  .toList(),
+              onPageChanged: (value) {},
+              autoPlayInterval: 60000,
+            ),
+            // Thông tin chi tiết xe
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: BookingdetailTest(
+                bikeModel: widget.bikeModel,
+                bookingModel: widget.bookingModel,
+                // isCustomerHistory: widget.isCustomerHistory,
+                // isCustomerHistoryDetail: widget.isCustomerHistoryDetail,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+}
---
+++ lib/widgets/test_booking_detail.dart
@@ -0,0 +1,364 @@
+import 'package:bike_for_rent/constants/api_url.dart';
+import 'package:bike_for_rent/models/bike_brand_model.dart';
+import 'package:bike_for_rent/models/bike_model.dart';
+import 'package:bike_for_rent/models/bike_type_model.dart';
+import 'package:bike_for_rent/models/booking_model.dart';
+import 'package:bike_for_rent/models/payment_type_model.dart';
+import 'package:bike_for_rent/services/bike_brand_service.dart';
+import 'package:bike_for_rent/services/bike_service.dart';
+import 'package:bike_for_rent/services/bike_type_service.dart';
+import 'package:bike_for_rent/services/payment_type_service.dart';
+import 'package:flutter/material.dart';
+import 'package:bike_for_rent/helper/helper.dart' as helper;
+import 'package:bike_for_rent/constants/my_colors.dart' as my_colors;
+
+class BookingdetailTest extends StatefulWidget {
+  final BookingModel bookingModel;
+  final BikeModel bikeModel;
+
+  BookingdetailTest({
+    Key key,
+    this.bookingModel,
+    this.bikeModel,
+  }) : super(key: key);
+
+  @override
+  _BookingdetailTestState createState() => _BookingdetailTestState();
+}
+
+class _BookingdetailTestState extends State<BookingdetailTest> {
+  // String bikeTypeStr = "";
+  // String bikeBrandStr = "";
+
+  // BikeService bikeService = new BikeService();
+  // BikeModel loadBikeById(String bikeId) {
+  //   BikeModel result;
+  //   Future<BikeModel> futureCases = bikeService.getBikeById(bikeId);
+  //   futureCases.then((model) {
+  //     result = model;
+  //   });
+  //   return result;
+  // }
+
+  // BikeTypeService bikeTypeService = new BikeTypeService();
+  // BikeTypeModel bikeTypeModel;
+  // Future loadBikeTypeById(String bikeTypeId) {
+  //   Future<BikeTypeModel> futureCases =
+  //       bikeTypeService.getBikeTypeById(bikeTypeId);
+  //   futureCases.then((model) {
+  //     bikeTypeModel = model;
+  //   });
+  //   return futureCases;
+  // }
+
+  // BikeBrandService bikeBrandService = new BikeBrandService();
+  // BikeBrandModel bikeBrandModel;
+  // Future loadBikeBrandById(String bikeBrandId) {
+  //   Future<BikeBrandModel> futureCases =
+  //       bikeBrandService.getBikeBrandById(bikeBrandId);
+  //   futureCases.then((model) {
+  //     bikeBrandModel = model;
+  //   });
+  //   return futureCases;
+  // }
+
+  // PaymentTypeService paymentTypeService = new PaymentTypeService();
+  // PaymentTypeModel paymentTypeModel;
+  // PaymentTypeModel loadPaymentTypeById(String paymentTypeId) {
+  //   PaymentTypeModel result;
+  //   Future<PaymentTypeModel> futureCases =
+  //       paymentTypeService.getPaymentTypeById(paymentTypeId);
+  //   futureCases.then((model) {
+  //     result = model;
+  //   });
+  //   return result;
+  // }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.bikeModel == null)
+      return Center(child: Text("Không có lịch sử thuê"));
+    else
+      return Column(
+        children: [
+          // Tên xe
+          Row(
+            children: [
+              Text("Tên xe: ",
+                  // + widget.bikeModel.model,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Loại xe: ",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        Expanded(
+                          child:
+                              // FutureBuilder(
+                              //   future: loadBikeTypeById(widget.bikeModel.typeId),
+                              //   builder: (context, snapshot) {
+                              //     Future.delayed(
+                              //       Duration(milliseconds: 1),
+                              //       () => bikeTypeStr = bikeTypeModel.name,
+                              //     );
+                              //     return
+                              Text(
+                            // widget.bikeModel.bikeTypeModel.name,
+                            "tên",
+                            style: TextStyle(fontSize: 15),
+                            //   );
+                            // },
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Hãng: ",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        Expanded(
+                          child:
+                              // FutureBuilder(
+                              //   future: loadBikeBrandById(widget.bikeModel.brandId),
+                              //   builder: (context, snapshot) {
+                              //     Future.delayed(
+                              //       Duration(milliseconds: 1),
+                              //       () => bikeBrandStr = bikeBrandModel.name,
+                              //     );
+                              //     return
+                              Text(
+                            // widget.bikeModel.bikeBrandModel.name,
+                            "hãng",
+                            style: TextStyle(fontSize: 15),
+                            //   );
+                            // },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Biển số xe: ",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        Expanded(
+                          child: Text(
+                              // widget.bikeModel.licensePlates,
+                              "biển",
+                              style: TextStyle(fontSize: 15)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Màu sắc: ",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold)),
+                        Expanded(
+                          child: Text(
+                              // widget.bikeModel.color,
+                              "màu",
+                              style: TextStyle(fontSize: 15)),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //   if (widget.isCustomerHistory && widget.isCustomerHistoryDetail)
+          //     Column(
+          //       children: [
+          //         SizedBox(height: 20),
+          //         Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Text("Ngày / giờ thuê xe: ",
+          //                 style: TextStyle(
+          //                     fontSize: 15, fontWeight: FontWeight.bold)),
+          //             Expanded(
+          //               child: Text(widget.bookingModel.dateBegin,
+          //                   style: TextStyle(fontSize: 15)),
+          //             ),
+          //           ],
+          //         ),
+          //         SizedBox(height: 10),
+          //         Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Text(widget.bookingModel.dateEnd,
+          //                 style: TextStyle(
+          //                     fontSize: 15, fontWeight: FontWeight.bold)),
+          //             Expanded(
+          //               child: Text("31/12/2021 - 15:30",
+          //                   style: TextStyle(fontSize: 15)),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+          //   if (widget.isCustomerHistory)
+          //     Column(
+          //       children: [
+          //         SizedBox(height: 20),
+          //         Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             if (widget.isCustomerHistoryDetail)
+          //               Expanded(
+          //                 child: Column(
+          //                   mainAxisAlignment: MainAxisAlignment.start,
+          //                   crossAxisAlignment: CrossAxisAlignment.start,
+          //                   children: [
+          //                     Row(
+          //                       mainAxisAlignment: MainAxisAlignment.start,
+          //                       crossAxisAlignment: CrossAxisAlignment.start,
+          //                       children: [
+          //                         Text("Thời gian thuê: ",
+          //                             style: TextStyle(
+          //                                 fontSize: 15,
+          //                                 fontWeight: FontWeight.bold)),
+          //                         Expanded(
+          //                           child: Text("1 ngày 10 tiếng",
+          //                               style: TextStyle(fontSize: 15)),
+          //                         ),
+          //                       ],
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ),
+          //             if (widget.isCustomerHistoryDetail) SizedBox(width: 10),
+          //             Expanded(
+          //               child: Column(
+          //                 mainAxisAlignment: MainAxisAlignment.start,
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Row(
+          //                     children: [
+          //                       Text("Trạng thái: ",
+          //                           style: TextStyle(
+          //                               fontSize: 15,
+          //                               fontWeight: FontWeight.bold)),
+          //                       Expanded(
+          //                         child: Text(
+          //                           helper.getStatus(widget
+          //                               .bookingModel.eventTypeId
+          //                               .toUpperCase()),
+          //                           style: TextStyle(
+          //                             fontSize: 15,
+          //                             color: (widget.bookingModel.eventTypeId
+          //                                         .toUpperCase() ==
+          //                                     "CANCELED")
+          //                                 ? my_colors.danger
+          //                                 : my_colors.primary,
+          //                           ),
+          //                         ),
+          //                       ),
+          //                     ],
+          //                   ),
+          //                 ],
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+          //   if (widget.isCustomerHistory)
+          //     Column(
+          //       children: [
+          //         SizedBox(height: 20),
+          //         Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           crossAxisAlignment: CrossAxisAlignment.start,
+          //           children: [
+          //             Expanded(
+          //               child: Column(
+          //                 mainAxisAlignment: MainAxisAlignment.start,
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Row(
+          //                     children: [
+          //                       Text("Thanh toán: ",
+          //                           style: TextStyle(
+          //                               fontSize: 15,
+          //                               fontWeight: FontWeight.bold)),
+          //                       Expanded(
+          //                         child: Text("Tiền mặt",
+          //                             style: TextStyle(fontSize: 15)),
+          //                       ),
+          //                     ],
+          //                   ),
+          //                 ],
+          //               ),
+          //             ),
+          //             SizedBox(width: 10),
+          //             Expanded(
+          //               child: Column(
+          //                 mainAxisAlignment: MainAxisAlignment.start,
+          //                 crossAxisAlignment: CrossAxisAlignment.start,
+          //                 children: [
+          //                   Row(
+          //                     children: [
+          //                       Text("Tổng tiền: ",
+          //                           style: TextStyle(
+          //                               fontSize: 15,
+          //                               fontWeight: FontWeight.bold)),
+          //                       Expanded(
+          //                         child: Text("123456 vnd",
+          //                             style: TextStyle(fontSize: 15)),
+          //                       ),
+          //                     ],
+          //                   ),
+          //                 ],
+          //               ),
+          //             ),
+          //           ],
+          //         ),
+          //       ],
+          //     ),
+        ],
+      );
+  }
+}
+// }
